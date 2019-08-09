d4ee0406187690431164967486e8b0023a06f109
= <=>
2019-08-09 11:24:07 +0800

    modefy.

--- src/main.go
+++ src/main.go
@@ -7,12 +7,6 @@ import (
 	xmlpath "gopkg.in/xmlpath.v2"
 )
 
-func main() {
-	nodename()
-	// getFieldValue()
-	// getSpecifiedValue()
-}
-
 var file *os.File
 var node *xmlpath.Node
 
@@ -22,35 +16,43 @@ func init() {
 	if err != nil {
 		panic("openFile failed!!!")
 	}
-	fmt.Println(file)
-	//解析文件获得经过处理的并且可以被path访问的Node类型的node
+
+	defer file.Close()
+
+	// 解析文件获得经过处理的并且可以被path访问的Node类型的node
 	node, err = xmlpath.Parse(file)
 	if err != nil {
-		panic("xmlpath parse file failed!!!")
+		panic("xmlpath parse file failed!!! ")
 	}
 }
 
+func main() {
+	nodename()
+	getFieldValue()
+	// getSpecifiedValue()
+}
+
 func nodename() {
-	//bookstore为根节点编译过后得到一个*Path类型的值
+	// bookstore为根节点编译过后得到一个*Path类型的值
 	path := xmlpath.MustCompile("bookstore")
-	//可能会有多本书所以使用path.Iter(node)获取该节点下面的node集合也就是iterator
+	// 可能会有多本书所以使用path.Iter(node)获取该节点下面的node集合也就是iterator
 	it := path.Iter(node)
-	//判断是否有下一个
+	// 判断是否有下一个
 	for it.Next() {
-		//如果有把当前的Node节点取出 并打印出值
+		// 如果有把当前的Node节点取出 并打印出值
 		fmt.Println(it.Node().String())
 	}
 }
 
 func getFieldValue() {
-	//选取全文中属性包含@lang的节点 而不管他的位置
+	// 选取全文中属性包含@lang的节点 而不管他的位置
 	path := xmlpath.MustCompile("//@lang")
 	it := path.Iter(node)
 	for it.Next() {
 		fmt.Println(it.Node().String())
 	}
 
-	//选取全文中属性包含lang并且值为en的节点 而不管他的位置
+	// 选取全文中属性包含lang并且值为en的节点 而不管他的位置
 	path = xmlpath.MustCompile("//*[@lang=\"en\"]")
 	it = path.Iter(node)
 	for it.Next() {
@@ -59,7 +61,7 @@ func getFieldValue() {
 }
 
 func getSpecifiedValue() {
-	//选取Bookstore中的第二本书的title节点
+	// 选取Bookstore中的第二本书的title节点
 	path := xmlpath.MustCompile("/bookstore/book[2]/title")
 	fmt.Println(path.String(node))
 }
--- src/train/fopen.go
+++ src/train/fopen.go
@@ -1,7 +1,10 @@
 package train
 
 import (
+	"bufio"
 	"fmt"
+	"io"
+	"os"
 	"strings"
 )
 
@@ -15,4 +18,81 @@ func Fopen() {
 		panic(err)
 	}
 	fmt.Printf("%s\n%d\n", p, n)
+}
+
+// ReadAllIntoMemory 整个文件读到内存，适用于文件较小的情况
+func ReadAllIntoMemory(filename string) (content []byte, err error) {
+	// 获取文件指针
+	fp, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer fp.Close()
+
+	fileInfo, err := fp.Stat()
+	if err != nil {
+		return nil, err
+	}
+
+	// 文件内容读取到buffer中
+	buffer := make([]byte, fileInfo.Size())
+	_, err = fp.Read(buffer)
+	if err != nil {
+		return nil, err
+	}
+
+	return buffer, nil
+}
+
+// readByBlock 一块一块地读取, 即给一个缓冲, 分多次读到缓冲中
+func readByBlock(filename string) (content []byte, err error) {
+	// 获取文件指针
+	fp, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer fp.Close()
+
+	// 缓冲大小, 每次读取64个字节
+	buffer := make([]byte, 64)
+	for {
+		// 注意这里要取bytesRead, 否则有问题
+		bytesRead, err := fp.Read(buffer) // 文件内容读取到buffer中
+		content = append(content, buffer[:bytesRead]...)
+		if err != nil {
+			if err == io.EOF {
+				err = nil
+				break
+			} else {
+				return nil, err
+			}
+		}
+	}
+	return
+}
+
+// readByLine 逐行读取, 一行是一个[]byte, 多行就是[][]byte
+func readByLine(filename string) (lines [][]byte, err error) {
+	// 获取文件指针
+	fp, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer fp.Close()
+	bufReader := bufio.NewReader(fp)
+
+	for {
+		// 按行读
+		line, _, err := bufReader.ReadLine()
+		if err != nil {
+			if err == io.EOF {
+				err = nil
+				break
+			}
+		} else {
+			lines = append(lines, line)
+		}
+	}
+
+	return
 }
