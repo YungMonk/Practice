dbfce8cdb75d28ec483e39afc88bb1ce521dcc48
Your Name <you@example.com>
2019-08-12 11:56:07 +0800

    paraser.

--- src/main.go
+++ src/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 
@@ -8,23 +9,10 @@ import (
 )
 
 var file *os.File
-var node *xmlpath.Node
 var htmlNode *xmlpath.Node
 
 func init() {
 	var err error
-	file, err = os.OpenFile("t.xml", os.O_RDWR, os.ModePerm)
-	if err != nil {
-		panic("openFile failed!!!")
-	}
-
-	defer file.Close()
-
-	// 解析文件获得经过处理的并且可以被path访问的Node类型的node
-	node, err = xmlpath.Parse(file)
-	if err != nil {
-		panic("xmlpath parse file failed!!! ")
-	}
 
 	file, err = os.OpenFile("carjob.html", os.O_RDWR, os.ModePerm)
 	if err != nil {
@@ -40,43 +28,78 @@ func init() {
 }
 
 func main() {
-	nodename()
-	// getFieldValue()
-	// getSpecifiedValue()
+	setConifg("/mnt/d/Development/workspace/src/config.json")
+	// nodename()
 }
 
+// ParserConfig init
+// config := parserConfig{
+// 	filed: "position",
+// 	rules: "//*[@id=\"resultList\"]/div[@class=\"el\"]",
+// 	child: []*parserConfig{
+// 		&parserConfig{filed: "name", rules: "./span[2]", child: nil},
+// 	},
+// }
+type ParserConfig struct {
+	Filed string          `json:"filed"`
+	Rules string          `json:"rules"`
+	Child []*ParserConfig `json:"child"`
+}
+
+// filed, rules, child
 func nodename() {
+
 	// bookstore为根节点编译过后得到一个*Path类型的值 //*[@id="resultList"]/div[7]
 	path := xmlpath.MustCompile("//*[@id=\"resultList\"]/div[@class=\"el\"]")
 
 	// 可能会有多本书所以使用path.Iter(node)获取该节点下面的node集合也就是iterator
 	it := path.Iter(htmlNode)
 
+	nodes := []*xmlpath.Node{}
 	// 判断是否有下一个
 	for it.Next() {
-		// 如果有把当前的Node节点取出 并打印出值
-		fmt.Println(it.Node().String())
+		nodes = append(nodes, it.Node())
 	}
+
+	childPath := xmlpath.MustCompile("./span[2]")
+	childStr, err := childPath.String(nodes[0])
+	if !err {
+		fmt.Printf("Parser rule is: %s\nResult: %s", "./span[1]", childStr)
+	}
+
+	fmt.Println(childStr)
 }
 
-func getFieldValue() {
-	// 选取全文中属性包含@lang的节点 而不管他的位置
-	path := xmlpath.MustCompile("//@lang")
-	it := path.Iter(node)
-	for it.Next() {
-		fmt.Println(it.Node().String())
+func setConifg(filename string) ParserConfig {
+	// 获取文件指针
+	fp, err := os.Open(filename)
+	if err != nil {
+		fmt.Println("config file is not exist.")
+	}
+	defer fp.Close()
+
+	fileInfo, err := fp.Stat()
+	if err != nil {
+		fmt.Println("config file is read error.")
 	}
 
-	// 选取全文中属性包含lang并且值为en的节点 而不管他的位置
-	path = xmlpath.MustCompile("//*[@lang=\"en\"]")
-	it = path.Iter(node)
-	for it.Next() {
-		fmt.Println(it.Node().String())
+	// 文件内容读取到buffer中
+	fileData := make([]byte, fileInfo.Size())
+	_, err = fp.Read(fileData)
+	if err != nil {
+		fmt.Println("config file is read faild.")
 	}
-}
 
-func getSpecifiedValue() {
-	// 选取Bookstore中的第二本书的title节点
-	path := xmlpath.MustCompile("/bookstore/book[2]/title")
-	fmt.Println(path.String(node))
+	fmt.Printf("%s \n", string(fileData))
+
+	var parCfg ParserConfig
+
+	err = json.Unmarshal(fileData, &parCfg)
+	if err != nil {
+		fmt.Printf("json export struct faild: %+v \n", err)
+	}
+	// fmt.Printf("%+v \n", parCfg)
+	// fmt.Printf("%+v \n", parCfg.Child[0])
+
+	return parCfg
 }
