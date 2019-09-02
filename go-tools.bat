c642796c6e700276f39bf73cd766fcf8679afe24
Your Name <you@example.com>
2019-09-02 18:01:05 +0800

    git submodule

--- go-tools.bat
+++ go-tools.bat
@@ -1,5 +1,6 @@
-mkdir  %GOPATH%\src\golang.org\x
-git clone https://github.com/golang/tools.git %GOPATH%\src\golang.org\x\tools
+MKDIR  src/golang.org/x
+
+git clone https://github.com/golang/tools.git src/golang.org/x/tools
 
 go get -v github.com/mdempsky/gocode
 go get -v github.com/uudashr/gopkgs/cmd/gopkgs
@@ -7,25 +8,26 @@ go get -v github.com/ramya-rao-a/go-outline
 go get -v github.com/acroca/go-symbols
 go get -v golang.org/x/tools/cmd/guru
 go get -v golang.org/x/tools/cmd/gorename
-go get -v github.com/derekparker/delve/cmd/dlv
+go get -v github.com/go-delve/delve/cmd/dlv
 go get -v github.com/stamblerre/gocode
 go get -v github.com/rogpeppe/godef
 go get -v github.com/ianthehat/godef
 go get -v github.com/sqs/goreturns
 go get -v github.com/golang/lint
-git clone https://github.com/golang/lint.git %GOPATH%\src\golang.org\x\lint
 
-go build -o %GOPATH%\bin\gocode.exe github.com/mdempsky/gocode
-go build -o %GOPATH%\bin\gopkgs.exe github.com/uudashr/gopkgs/cmd/gopkgs
-go build -o %GOPATH%\bin\go-outline.exe github.com/ramya-rao-a/go-outline
-go build -o %GOPATH%\bin\go-symbols.exe github.com/acroca/go-symbols
-go build -o %GOPATH%\bin\guru.exe golang.org/x/tools/cmd/guru
-go build -o %GOPATH%\bin\gorename.exe golang.org/x/tools/cmd/gorename
-go build -o %GOPATH%\bin\dlv.exe github.com/derekparker/delve/cmd/dlv
-go build -o %GOPATH%\bin\gocode-gomod.exe github.com/stamblerre/gocode
-go build -o %GOPATH%\bin\godef.exe github.com/rogpeppe/godef
-go build -o %GOPATH%\bin\godef-gomod.exe github.com/ianthehat/godef
-go build -o %GOPATH%\bin\goreturns.exe github.com/sqs/goreturns
-go build -o %GOPATH%\bin\golint.exe golang.org/x/lint/golint
+git clone https://github.com/golang/lint.git src/golang.org/x/lint
 
-pause
+go build -o bin/gocode.exe github.com/mdempsky/gocode
+go build -o bin/gopkgs.exe github.com/uudashr/gopkgs/cmd/gopkgs
+go build -o bin/go-outline.exe github.com/ramya-rao-a/go-outline
+go build -o bin/go-symbols.exe github.com/acroca/go-symbols
+go build -o bin/guru.exe golang.org/x/tools/cmd/guru
+go build -o bin/gorename.exe golang.org/x/tools/cmd/gorename
+go build -o bin/dlv.exe github.com/go-delve/delve/cmd/dlv
+go build -o bin/gocode-gomod.exe github.com/stamblerre/gocode
+go build -o bin/godef.exe github.com/rogpeppe/godef
+go build -o bin/godef-gomod.exe github.com/ianthehat/godef
+go build -o bin/goreturns.exe github.com/sqs/goreturns
+go build -o bin/golint.exe golang.org/x/lint/golint
+
+PAUSE
